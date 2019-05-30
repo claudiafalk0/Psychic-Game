54fa5a2642f45f9468b83f2ff8dc8828f2827eed
Claudia Falk <claudiafalk0@gmail.com>
2019-05-30 12:49:24 -0600

    added for loops to js, added background image to css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,29 +12,36 @@ document.onkeyup = function(event){
     var guessSoFar = event.key.toLowerCase();
 
     var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
-
-    for (var i = 16; i > 0; i--) {
-        guessLeftText = i;
-    } 
-
-    for( var i = 1; i <=16; i++) {
-        userGuess.push(guessHistory);
-    }
     
-    for (var j = 0; j <userGuess.length; j++){
-        UserGuessText.textContent = userGuess[j];
-    }
-    
-    if (guessSoFar === computerGuess && guessLeftText > 0) {
-        wins++
-        alert("Yay you guessed right! The computer guessed: " + computerGuess)
+    if (guessSoFar === guessHistory){
+        alert ("Please choose a different letter!");
     } else {
-        losses++
-        alert("Oh no! I'm sorry, the computer guessed: " + computerGuess)
+
+        for (var i = 16; i > 0; i--) {
+            guessLeftText.textContent = i;
+        } 
+        
+        for( var k = 1; k <=16; k++) {
+            guessHistory = guessSoFar;
+            userGuess.push(guessHistory);
+        }
+        
+        for (var j = 0; j <userGuess.length; j++){
+            UserGuessText.textContent = userGuess[j];
+        }
+        
+        if (guessSoFar === computerGuess && guessLeftText > 0) {
+            wins++
+            alert("Yay you guessed right! The computer guessed: " + computerGuess)
+        } else {
+            losses++
+            alert("Oh no! I'm sorry, the computer guessed: " + computerGuess)
+        }
     }
+
+    
+    
 
     winsText.textContent = wins;
     lossesText.textContent = losses;
-    UserGuessText.textContent = guessSoFar;
-    guessLeftText.textContent = guessLeft;
 }
